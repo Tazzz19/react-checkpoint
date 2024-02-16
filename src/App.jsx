9d883c56339f384bba551f07fc7562fce5f6a39b
Tazzz19 <hamzatrahman92@gmail.com>
2024-02-16 12:27:12 +0100

    Done

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import NavBar from './components/NavBar'
+import Body from './components/Body'
+import Footer from './components/Footer'
+
+function App() {
+  return (
+    <>
+    <NavBar/>
+    <Body />
+    <Footer /> 
+    </>
+  )
+}
+
+export default App
